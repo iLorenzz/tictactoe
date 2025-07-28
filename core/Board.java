d446a91c8a9d072e90d844959a3bb531cfb6a0fe
iLorenzz <ldutra0912@gmail.com>
2025-07-27 23:03:06 -0300

    feat: implement method to find a winner or if it's a draw

--- core/Board.java
+++ core/Board.java
@@ -3,6 +3,7 @@ package core;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Board{
 	public static final int SIZE = 3;
@@ -48,5 +49,95 @@ public class Board{
 		}
 		
 		return sw.toString();
+	}
+	
+	public Symbol update(Symbol symbol, Coord coord){
+		Objects.requireNonNull(symbol);
+		Objects.requireNonNull(coord);
+		
+		if(symbol == Symbol.NONE){
+			throw new IllegalArgumentException("None cannot be add to board");
+		}
+		
+		if(matrix[coord.i()][coord.j()] != Symbol.NONE){
+			throw new IllegalArgumentException("Play is not possible, position already has been played");
+		}
+		
+		matrix[coord.i()][coord.j()] = symbol;
+		return findSequence();
+	}
+	
+	public boolean isFull(){
+		for(int i = 0; i < SIZE; i++){
+			for(int j = 0; j < SIZE; j++){
+				if(matrix[i][j] == Symbol.NONE){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	private Symbol findSequence(){
+		Symbol symbol = findSequenceInRows();
+		if(symbol != null){
+			return symbol;
+		}
+		symbol = findSequenceInColumns();
+		if(symbol != null){
+			return symbol;
+		}
+		return findSequenceInDiagonals();
+	}
+	
+	private Symbol findSequenceInRows(){
+		for(int i = 0; i < SIZE; i++){
+			Symbol symbol = findSequenceInRow(i);
+			if(symbol != null){
+				return symbol;
+			}
+		}
+		return null;
+	}
+	
+	private Symbol findSequenceInRow(int i){
+		return matrix[i][0] == matrix[i][1] 
+		&& matrix[i][1] == matrix[i][2] 
+		&& matrix[i][0] != Symbol.NONE
+		? matrix[i][0] : null;
+	}
+	
+	private Symbol findSequenceInColumns(){
+		for(int j = 0; j < SIZE; j++){
+			Symbol symbol = findSequenceInRow(j);
+			if(symbol != null){
+				return symbol;
+			}
+		}
+		return null;
+	}
+	
+	private Symbol findSequenceInColum(int j){
+		return matrix[0][j] == matrix[1][j] 
+		&& matrix[1][j] == matrix[2][j] 
+		&& matrix[0][j] != Symbol.NONE
+		? matrix[0][j] : null;
+	}
+	
+	private Symbol findSequenceInDiagonals(){
+		if(matrix[0][0] == matrix[1][1]
+			&& matrix[1][1] == matrix[2][2]
+			&& matrix[0][0] != Symbol.NONE
+		){
+			return matrix[0][0];
+		}
+		
+		if(matrix[0][2] == matrix[1][1]
+			&& matrix[1][1] == matrix[2][0]
+			&& matrix[0][2] != Symbol.NONE
+		){
+			return matrix[0][2];
+		}
+		
+		return null;
 	}
 }
--- core/Game.java
+++ core/Game.java
@@ -8,20 +8,31 @@ public class Game{
 	private final Players players = new Players();
 	
 	public void start() {
-		while(true){
+		Symbol winner = null;
+		
+		while(winner == null && !board.isFull()){
+			
 			Output.writeNewLine();
 			Output.write(board);
-			play(players.next());
+			winner = play(players.next());
 		}
+		
+		Output.write(board);
+		
+		if(board.isFull()){
+			Output.write("It's a draw!");
+			return;
+		}
+		
+		Output.write(String.format("'%s' won!", winner));
 	}
 	
-	private void play(Symbol symbol){
+	private Symbol play(Symbol symbol){
 		while(true){
 			try{
 				String play = Input.read(String.format("'%s' play ==> ", symbol));
 				Coord coord = Coord.parse(play);
-				board.update(symbol, coord);
-				break;
+				return board.update(symbol, coord);
 			}catch(RuntimeException re){
 				Output.write("ERROR: " + re);
 			}

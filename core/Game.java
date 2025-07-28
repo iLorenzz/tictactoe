81552d51e28e28cd2f99993896a0787312ad2c07
iLorenzz <ldutra0912@gmail.com>
2025-07-27 21:43:33 -0300

    feat: implement the game run

--- core/Game.java
+++ core/Game.java
@@ -1,10 +1,30 @@
 package core;
 
+import io.Input;
+import io.Output;
+
 public class Game{
 	private final Board board = new Board();
 	private final Players players = new Players();
 	
 	public void start() {
-		System.out.println(board);
+		while(true){
+			Output.writeNewLine();
+			Output.write(board);
+			play(players.next());
+		}
+	}
+	
+	private void play(Symbol symbol){
+		while(true){
+			try{
+				String play = Input.read(String.format("'%s' play ==> ", symbol));
+				Coord coord = Coord.parse(play);
+				board.update(symbol, coord);
+				break;
+			}catch(RuntimeException re){
+				Output.write("ERROR: " + re);
+			}
+		}
 	}
 }
